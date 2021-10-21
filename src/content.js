0c8f9cfbe34643bf96b66e3b76c9b386cbbeae1e
Clement Walter <clement.walter@ibm.com>
2021-10-21 11:25:05 +0200

    Fix some regex and allow for multi matches

--- src/content.js
+++ src/content.js
@@ -1,21 +1,24 @@
 // constants
-const HTML_SPACE_REGEX = /[\u00a0 ]/g;
 const NUMBER_REGEX = /(\d+[\d\s]*[.,]?[\de-]*)/;
 const CURRENCY_REGEX =
   /[$\xA2-\xA5\u058F\u060B\u09F2\u09F3\u09FB\u0AF1\u0BF9\u0E3F\u17DB\u20A0-\u20BD\uA838\uFDFC\uFE69\uFF04\uFFE0\uFFE1\uFFE5\uFFE6]/;
 const PRICE_REGEX = `(${CURRENCY_REGEX.source}${NUMBER_REGEX.source})|(${NUMBER_REGEX.source}${CURRENCY_REGEX.source})`;
 
 // utils
-const updateContent = (rate) => (textContent) => {
-  const price = textContent.replace(HTML_SPACE_REGEX, "").match(PRICE_REGEX);
-  if (!price) {
-    return textContent;
-  }
-  const number = parseFloat(price[0].match(NUMBER_REGEX)[0]);
-  return textContent
-    .replace(NUMBER_REGEX, number / rate.BTCUSDT)
-    .replace(CURRENCY_REGEX, "BTC");
-};
+const updateContent = (rate) => (textContent) =>
+  Array.from(textContent.matchAll(PRICE_REGEX))
+    .map((price) => [
+      price[0],
+      `${
+        parseFloat(price[0].match(NUMBER_REGEX)[0].replace(" ", "")) /
+        rate.BTCUSDT
+      } BTC`,
+    ])
+    .reduce(
+      (previousValue, currentValue) =>
+        previousValue.replace(currentValue[0], currentValue[1]),
+      textContent
+    );
 
 const updateDOM = (rate) => {
   const allElements = document.getElementsByTagName("*");
