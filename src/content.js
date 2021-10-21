11e18a5ff8f82ebbd317983afbf2cc790696fcfe
Clement Walter <clement.walter@ibm.com>
2021-10-21 18:51:58 +0200

    Move regex to constants file

--- src/content.js
+++ src/content.js
@@ -1,8 +1,4 @@
-// constants
-const NUMBER_REGEX = /(\d+[\d\s,]*\.?[\de-]*)/;
-const CURRENCY_REGEX =
-  /[$\xA2-\xA5\u058F\u060B\u09F2\u09F3\u09FB\u0AF1\u0BF9\u0E3F\u17DB\u20A0-\u20BD\uA838\uFDFC\uFE69\uFF04\uFFE0\uFFE1\uFFE5\uFFE6]/;
-const PRICE_REGEX = `(${CURRENCY_REGEX.source}${NUMBER_REGEX.source})|(${NUMBER_REGEX.source}${CURRENCY_REGEX.source})`;
+import { PRICE_REGEX, NUMBER_REGEX } from "./constants";
 
 // utils
 const updateContent = (rate) => (textContent) =>
