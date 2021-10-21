85c96436568679b7473f1a6ec1c48998a433ae80
Clement Walter <clement.walter@ibm.com>
2021-10-21 20:26:06 +0200

    Add test for all regex

--- src/constants.test.js
+++ src/constants.test.js
@@ -1,27 +1,39 @@
-import { NUMBER_REGEX } from "./constants";
+import { NUMBER_REGEX, PRICE_REGEX, CURRENCY_REGEX } from "./constants";
 
-describe("NUMBER_REGEX", () => {
+describe("Regex", () => {
   it.each`
-    template                 | match
-    ${"From 4999 only"}      | ${"4999"}
-    ${"From $4999 only"}     | ${"4999"}
-    ${"From 4999$ only"}     | ${"4999"}
-    ${"From 4,999 only"}     | ${"4,999"}
-    ${"From $4,999 only"}    | ${"4,999"}
-    ${"From 4.999$ only"}    | ${"4.999"}
-    ${"From 4 999 only"}     | ${"4 999"}
-    ${"From $4 999 only"}    | ${"4 999"}
-    ${"From 4 999$ only"}    | ${"4 999"}
-    ${"From 4999.50 only"}   | ${"4999.50"}
-    ${"From $4999.50 only"}  | ${"4999.50"}
-    ${"From 4999.50$ only"}  | ${"4999.50"}
-    ${"From 4,999.50 only"}  | ${"4,999.50"}
-    ${"From $4,999.50 only"} | ${"4,999.50"}
-    ${"From 4.999.50$ only"} | ${"4.999.50"}
-    ${"From 4 999.50 only"}  | ${"4 999.50"}
-    ${"From $4 999.50 only"} | ${"4 999.50"}
-    ${"From 4 999.50$ only"} | ${"4 999.50"}
-  `("should match a single number", ({ template, match }) => {
-    expect(template.match(NUMBER_REGEX)[0]).toEqual(match);
-  });
+    template                 | matchNumber   | matchPrice     | matchCurrency
+    ${"From 4999 only"}      | ${"4999"}     | ${null}        | ${null}
+    ${"From $4999 only"}     | ${"4999"}     | ${"$4999"}     | ${"$"}
+    ${"From 4999$ only"}     | ${"4999"}     | ${"4999$"}     | ${"$"}
+    ${"From 4,999 only"}     | ${"4,999"}    | ${null}        | ${null}
+    ${"From $4,999 only"}    | ${"4,999"}    | ${"$4,999"}    | ${"$"}
+    ${"From 4.999$ only"}    | ${"4.999"}    | ${"4.999$"}    | ${"$"}
+    ${"From 4 999 only"}     | ${"4 999"}    | ${null}        | ${null}
+    ${"From $4 999 only"}    | ${"4 999"}    | ${"$4 999"}    | ${"$"}
+    ${"From 4 999$ only"}    | ${"4 999"}    | ${"4 999$"}    | ${"$"}
+    ${"From 4999.50 only"}   | ${"4999.50"}  | ${null}        | ${null}
+    ${"From $4999.50 only"}  | ${"4999.50"}  | ${"$4999.50"}  | ${"$"}
+    ${"From 4999.50$ only"}  | ${"4999.50"}  | ${"4999.50$"}  | ${"$"}
+    ${"From 4,999.50 only"}  | ${"4,999.50"} | ${null}        | ${null}
+    ${"From $4,999.50 only"} | ${"4,999.50"} | ${"$4,999.50"} | ${"$"}
+    ${"From 4.999.50$ only"} | ${"4.999.50"} | ${"4.999.50$"} | ${"$"}
+    ${"From 4 999.50 only"}  | ${"4 999.50"} | ${null}        | ${null}
+    ${"From $4 999.50 only"} | ${"4 999.50"} | ${"$4 999.50"} | ${"$"}
+    ${"From 4 999.50$ only"} | ${"4 999.50"} | ${"4 999.50$"} | ${"$"}
+    ${"From $ only"}         | ${null}       | ${null}        | ${"$"}
+  `(
+    "should match number, currency and price",
+    ({ template, matchNumber, matchPrice, matchCurrency }) => {
+      let number = template.match(NUMBER_REGEX);
+      number = number ? number[0] : number;
+      expect(number).toEqual(matchNumber);
+      let price = template.match(PRICE_REGEX);
+      price = price ? price[0] : price;
+      expect(price).toEqual(matchPrice);
+      let currency = template.match(CURRENCY_REGEX);
+      currency = currency ? currency[0] : currency;
+      expect(currency).toEqual(matchCurrency);
+    }
+  );
 });
